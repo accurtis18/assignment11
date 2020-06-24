b6637c8524c0c43216f8efc43b403538b6b46d71
Alex <achriscurtis@gmail.com>
2020-06-23 23:16:04 -0500

    completed api routes

--- routes.js
+++ routes.js
@@ -1,5 +1,32 @@
+const notes = require('./db.json');
+let id = 1;
+
 exports.route = (app) => {
-    app.get('api/notes', function(req, res) {
-        res.send("Got Notes");
+    app.get('/api/notes', function(req, res) {
+        return res.json(notes);
+      });
+
+      app.post("/api/notes", function(req, res) {
+        let newNote = {
+            "id": id,
+            "note": req.body.data
+        }
+        id++;
+        console.log(newNote);
+        console.log(req.body);
+      
+        notes.push(newNote);
+      
+        res.json(newNote);
+      });
+
+      app.delete("/api/notes/:id", function(req, res){
+        let deleteId = parseInt(req.params.id);
+        console.log(deleteId);
+        var arrayIndex = notes.findIndex(x => x.id === deleteId);
+        if (arrayIndex > -1){
+            notes.splice(arrayIndex, 1);
+        }
+        res.send(`successfully deleted note ${deleteId}`);
       });
 }
